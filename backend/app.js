82fde21d70e019af4d415de8eeb8fdd67cce505e
Facundo Rossi <rossifacundo94@gmail.com>
2018-05-18 19:41:16 -0300

    Expose Api get boards and list

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,15 @@
 var express = require('express')
 var database = require('./config/database')
 var startup = require('./startup/startupProperty')
+var BoardController = require('./controller/BoardController');
+var ListController = require('./controller/ListController');
 
 var app = express()
 
 startup.createPropertyByDefault()
+
+
+app.use('/boards', BoardController);
+app.use('/lists', ListController);
 
 module.exports = app
